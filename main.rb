6c39e2aa5f9e5cbe82394d316f8769f83ba84691
Yaromir <yaromirmorgan@gmail.com>
2018-01-19 18:25:04 +0300

    Generate random number

--- main.rb
+++ main.rb
@@ -4,6 +4,12 @@
 puts "Welcom to 'Get My Number!'"
 print 'Whats your name? '
 
-input = gets
+input = gets.chomp
+puts "Welcome, #{input}!"
 
-puts "Welcome, #{input}" 
+#generate rand number
+
+puts 'I got a random number between 1 and 100.'
+puts 'Can you guess it?'
+target = rand(1..100)
+ 
